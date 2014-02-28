da68f4f09d79a3b8b5e7804fe89dffcf91b1c326
dmtta <mastd863@newschool.edu>
2014-02-28 14:52:18 -0500

    First March Commit

---
+++ bw_SpaceMedusa/Spacewalk/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//transform.Rotate(Vector3.up);
+		//transform.Translate(Vector3.forward * 0.01f);
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Rotate(Vector3.left*.5f);
+
+			}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate(Vector3.down*.5f);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Rotate(Vector3.right*.5f);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(Vector3.up*.5f);
+		}
+		if (Input.GetKey ("space")){
+			rigidbody.AddForce(target.forward * 0.2f);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate(new Vector3(0,0,.5f));
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate(new Vector3(0,0,-.5f));
+		}
+		
+		if (Input.GetKey (KeyCode.F)) {
+			rigidbody.angularDrag=.6f;
+			rigidbody.drag=.6f;
+		}else{
+			rigidbody.drag=0;
+			rigidbody.angularDrag=0;
+		}
+	}
+
+
+}
---
+++ bw_SpaceMedusa/Spacewalk/Assets/Scripts/MouseControl.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var horizontalSpeed : float;
+var verticalSpeed : float;
+
+function Start () {
+	horizontalSpeed = 2;
+	verticalSpeed = 2;
+}
+ 
+function FixedUpdate (){
+    // Get the mouse delta. This is not in the range -1...1
+ 
+    var y : float = - horizontalSpeed * Input.GetAxis ("Mouse X");
+    var x : float = - verticalSpeed * Input.GetAxis ("Mouse Y");
+ 
+//    transform.Translate (x, 0, y);
+	transform.Rotate (x, 0, y);
+//	rigidbody.AddForce(x, 0, y);
+//	var localForce : Vector3 = camera.TransformDirection(new Vector3(x, 0, y));
+//	localForce.y = 0;
+//	rigidbody.AddForce(localForce);
+}
